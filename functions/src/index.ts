05983a4545c06caf82b33e372cec402d3252b02e
HarryHeckmann <heckmann.harry@gmail.com>
2022-06-28 10:27:43 -0500

    updated firebase.json

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
+
+const functions = require('firebase-functions')
+const express = require('express')
+const app = express()
+app.get('*', (req: any, res: any) => {
+  res.send("Hello from the API")
+})
+exports.api = functions.https.onRequest(app)
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
